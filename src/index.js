4dedee851d296343e7633939479365a15497aaa2
fereshte <fereshte.bahramy@gmail.com>
2022-01-02 23:38:57 +0330

    Update the time

--- src/index.js
+++ src/index.js
@@ -1,18 +1,34 @@
-function fomatDate(timesTemp) {
-  //calculate the date
-  return "Friday 05:00";
+//calculate the date
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
-  console.log(response.data);
-  console.log(response.data.main.temp);
   let temperatureElemnt = Math.round(response.data.main.temp);
   let temperature = document.querySelector("#Weather-summary");
   temperature.innerHTML = `${temperatureElemnt}`;
   let h1 = document.querySelector("#currentCity");
   h1.innerHTML = response.data.name;
-  let dateElement = document.querySelector("#date");
-  dateElement.innerHTML = fomatDate(response.data.dt * 1000);
   let description = document.querySelector("#weather-description");
   description.innerHTML = response.data.weather[0].description;
   let humidityElement = response.data.main.humidity;
@@ -21,6 +37,8 @@ function displayTemperature(response) {
   let windSpeedElement = Math.round(response.data.wind.speed);
   let windSpeed = document.querySelector("#wind-speed");
   windSpeed.innerHTML = `${windSpeedElement} km/h`;
+  let dateElement = document.querySelector("#date");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "33bb3131faab8b8de402456e4193a0d1";
